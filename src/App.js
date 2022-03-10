f1885d3a47ae8c9c7b5a4c2cfb5bea5ec9419849
Rashidi <prashidi@38f9d364d011.ant.amazon.com>
2022-03-10 15:54:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Amplify from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import AllPosts from "./containers/AllPosts";
+import PostsBySpecifiedUser from "./containers/PostsBySpecifiedUser";
+
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+const drawerWidth = 240;
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#1EA1F2",
+      contrastText: "#fff",
+    },
+    background: {
+      default: "#15202B",
+      paper: "#15202B",
+    },
+    divider: "#37444C",
+  },
+  overrides: {
+    MuiButton: {
+      color: "white",
+    },
+  },
+  typography: {
+    fontFamily: ["Arial"].join(","),
+  },
+  status: {
+    danger: "orange",
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "100%",
+    width: 800,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
+
+const App = () => {
+  const classes = useStyles()
+
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className={classes.root}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <HashRouter>
+              <Switch>
+                <Route exact path="/" component={AllPosts} />
+                <Route exact path="/global-timeline" component={AllPosts} />
+                <Route exact path="/:userId" component={PostsBySpecifiedUser} />
+                <Redirect path="*" to="/" />
+              </Switch>
+            </HashRouter>
+          </ThemeProvider>
+        </div>
+      )}
+    </Authenticator>
+  );
+};
+
+export default App;
